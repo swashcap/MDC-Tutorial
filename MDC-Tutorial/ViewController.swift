c6ba6ecbd26f38c565ab3ca952ddadd514df4ce5
Cory Reed <swashcap@gmail.com>
2017-11-13 09:40:26 -0800

    feat(pkg): install MaterialDesign components

--- MDC-Tutorial/ViewController.swift
+++ MDC-Tutorial/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MaterialComponents.MaterialCollections
 
 class ViewController: UIViewController {
 
---
+++ Pods/Target Support Files/Pods-MDC-TutorialUITests/Pods-MDC-TutorialUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MDC_TutorialUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MDC_TutorialUITests
+@end
