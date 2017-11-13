bac38cb0a1d1886afa1a879ebb75d5652a16082c
Cory Reed <swashcap@gmail.com>
2017-11-13 11:25:10 -0800

    fix(pkg): copy tutorial source code

--- MDC-Tutorial/ViewController.swift
+++ MDC-Tutorial/ViewController.swift
@@ -22,25 +22,34 @@ class ViewController: MDCCollectionViewController {
         addChildViewController(appBar.headerViewController)
         appBar.headerViewController.headerView.backgroundColor = UIColor(red: 1.0, green: 0.76, blue: 0.03, alpha: 1.0)
         appBar.headerViewController.headerView.trackingScrollView = self.collectionView
-        appBar.navigationBar.tintColor = UIColor.black
         appBar.addSubviewsToParent()
 
         title = "Material Components"
-    
         
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Edit",
-                                                            style: .plain,
-                                                            target: self,
-                                                            action: #selector(ViewController.barButtonDidTap))
-        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Edit", style: .plain, target: self, action: #selector(ViewController.barButtonDidTap(_:)))
+
+        appBar.navigationBar.tintColor = UIColor.black
+
         view.addSubview(fab)
         fab.translatesAutoresizingMaskIntoConstraints = false
         fab.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16.0).isActive = true
         fab.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -16.0).isActive = true
         fab.setTitle("+", for: .normal)
         fab.setTitle("-", for: .selected)
-        fab.addTarget(self, action: #selector(ViewController.fabDidTap), for: .touchUpInside)
+        fab.addTarget(self, action: #selector(ViewController.fabDidTap(_:)), for: .touchUpInside)
     }
+
+    func barButtonDidTap(sender: UIBarButtonItem) {
+        editor.isEditing = !editor.isEditing
+
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: editor.isEditing ? "Cancel" : "Edit", style: .plain, target: self, action: #selector(ViewController.barButtonDidTap(_:)))
+    }
+
+    func fabDidTap(sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+    }
+
+    // MARK: UICollectionViewDataSource
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 5
@@ -54,12 +63,16 @@ class ViewController: MDCCollectionViewController {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
 
         if let textCell = cell as? MDCCollectionViewTextCell {
+
+            // Add some mock text to the cell.
             let animals = ["Lions", "Tigers", "Bears", "Monkeys"]
             textCell.textLabel?.text = animals[indexPath.item]
         }
 
         return cell
     }
+
+    // MARK: UIScrollViewDelegate
 
     override func scrollViewDidScroll(_ scrollView: UIScrollView) {
         if scrollView == appBar.headerViewController.headerView.trackingScrollView {
@@ -73,17 +86,17 @@ class ViewController: MDCCollectionViewController {
         }
     }
 
-    func barButtonDidTap(sender: UIBarButtonItem) {
-        editor.isEditing = !editor.isEditing
-        
-        let buttonTitle =  editor.isEditing ? "Cancel" : "Edit"
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: buttonTitle,
-                                                            style: .plain,
-                                                            target: self,
-                                                            action: #selector(ViewController.barButtonDidTap))
+    override func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        if scrollView == appBar.headerViewController.headerView.trackingScrollView {
+            let headerView = appBar.headerViewController.headerView
+            headerView.trackingScrollDidEndDraggingWillDecelerate(decelerate)
+        }
     }
 
-    func fabDidTap(sender: UIButton) {
-        sender.isSelected = !sender.isSelected
+    override func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        if scrollView == appBar.headerViewController.headerView.trackingScrollView {
+            let headerView = appBar.headerViewController.headerView
+            headerView.trackingScrollWillEndDragging(withVelocity: velocity, targetContentOffset: targetContentOffset)
+        }
     }
 }
