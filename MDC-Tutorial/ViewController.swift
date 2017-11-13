2bca1b49496f9fa130117b373df6f415e2ed600f
Cory Reed <swashcap@gmail.com>
2017-11-13 09:11:43 -0800

    Initial Commit

---
+++ MDC-Tutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MDC-Tutorial
+//
+//  Created by Cory Reed on 11/13/17.
+//  Copyright © 2017 Cory Reed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
