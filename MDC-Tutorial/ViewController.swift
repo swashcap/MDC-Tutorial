a8713538a7c48b46485cc234ed7494bee110db33
Cory Reed <swashcap@gmail.com>
2017-11-13 10:13:03 -0800

    feat(pkg): wire up collection view, mock data

--- MDC-Tutorial/ViewController.swift
+++ MDC-Tutorial/ViewController.swift
@@ -9,18 +9,28 @@
 import UIKit
 import MaterialComponents.MaterialCollections
 
-class ViewController: UIViewController {
-
+class ViewController: MDCCollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        styler.cellStyle = .card
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 5
     }
 
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
 
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+
+        if let textCell = cell as? MDCCollectionViewTextCell {
+            let animals = ["Lions", "Tigers", "Bears", "Monkeys"]
+            textCell.textLabel?.text = animals[indexPath.item]
+        }
+
+        return cell
+    }
 }
-
